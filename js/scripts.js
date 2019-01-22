5bc096648e44fdf0be84df7e1fff1e73a1258e7f
Aurora Shido-Wagenet and Robbie Kruszynski <aurora@>
2019-01-22 15:42:30 -0800

    add loop functinality

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(function(){
+  $("#submit").click(function(){
+
+    var userInput = $("#sentence").val();
+    var inputArray = userInput.split(" ");
+    var longArray = [];
+
+    for (var index = 0; index < inputArray.length; index += 1) {
+      if (inputArray[index].length >= 3){
+        longArray.push(inputArray[index]);
+      };
+    };
+
+    longArray.reverse();
+    alert(longArray);
+
+  });
+});
